e03623646f8d7796cd51a07a8a1b5303edb498d8
jhonexu <jhonexu@tencent.com>
2021-06-13 23:41:41 +0800

    添加初始文件

--- ParserDemo/ViewController.m
+++ ParserDemo/ViewController.m
@@ -6,16 +6,36 @@
 //
 
 #import "ViewController.h"
-
+#import <Masonry.h>
+#import <Parse/Parse.h>
 @interface ViewController ()
-
+@property (nonatomic, strong) UITextView *titleView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.titleView = [[UITextView alloc] init];
+    self.titleView.font =  [UIFont systemFontOfSize:30.f];;
+    self.titleView.text = @"Parse 测试";
+    [self.view addSubview:self.titleView];
+  
+    [self.titleView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(self.view.mas_left).offset(15);
+        make.top.mas_equalTo(self.view.mas_top).offset(80);
+        make.width.height.mas_equalTo(300.0f);
+    }];
+    
+    [self initParser];
+}
+
+- (void)initParser {
+    [Parse initializeWithConfiguration:[ParseClientConfiguration configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+            configuration.applicationId = @"parseAppId";
+            configuration.clientKey = @"parseClientKey";
+            configuration.server = @"parseServerUrlString";
+        }]];
 }
 
 
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+#source=>'https://github.com/CocoaPods/Specs.git'#
+target 'ParserDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  #use_frameworks!
+  # Pods for ParserDemo
+  pod 'Parse'
+ # pod 'Parse/FacebookUtils'
+  pod 'Parse/TwitterUtils' 
+  pod 'Parse/UI'
+ pod 'Masonry'
+
+
+end
+
+  target 'ParserDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ParserDemoUITests' do
+    # Pods for testing
+  end
